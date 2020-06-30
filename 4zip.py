6cf1e9cf52404820c6d4125fdcb3744f4b128eec
jiayuxin <13520723393@163.com>
2020-06-30 16:30:42 +0800

    使用正则提取文件名特征并做对比

--- 4zip.py
+++ 4zip.py
@@ -7,26 +7,40 @@ import sys
 import zipfile
 
 nameMap = {}
+the_reg = re.compile('(.{2,4}?).*?([789七八九])([上下]).+?(\d{1,2})')
+ignore = ('zip')
+package_size = 1
 
 
-tt = re.compile('[789七八九]')
-
-
-def findSignature(name: str):
-    the_reg = re.compile('(.{2,4}?)版?([789七八九])([上下]).+?(\d{1,2})')
+def findSignature(name: str) -> tuple:
     m = the_reg.match(name)
-    return (m[0], m[1], m[2], m[3])
+    if m:
+        return (m[1], m[2], m[3], m[4])
+    else:
+        return ()
 
 
-# for file in os.listdir('./'):
-#     if file in __file__:
-#         continue
-#     name, ext = os.path.splitext(file)
-#     if name not in nameMap:
-#         nameMap[name] = []
-#     nameMap[name].append(file)
-# for name, files in nameMap.items():
-#     z = zipfile.ZipFile(name+'.zip', 'w', zipfile.ZIP_DEFLATED)
-#     for file in files:
-#         z.write(file)
-#     z.close()
+def Signature2String(sig: tuple) -> str:
+    return f'{sig[0]}版{sig[1]}{sig[2]}U{sig[3]}'
+
+
+for file in os.listdir('./'):
+    if file in __file__:
+        continue
+    _, ext = os.path.splitext(file)
+    if ext in ignore:
+        continue
+    name = findSignature(file)
+    if not name:
+        continue
+    if name not in nameMap:
+        nameMap[name] = []
+    nameMap[name].append(file)
+for name, files in nameMap.items():
+    if len(files) >= package_size:
+        continue
+    z = zipfile.ZipFile(Signature2String(name)+'.zip',
+                        'w', zipfile.ZIP_DEFLATED)
+    for file in files:
+        z.write(file)
+    z.close()
--- zip.py
+++ zip.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 import re
 
-the_reg = re.compile('(.{2,4}?)版?([789七八九])([上下]).+?(\d{1,2})')
-
+the_reg = re.compile('(.{2,4}?).*?([789七八九])([上下]).+?(\d{1,2})')
+content = '冀教版ASDF七下U7--雷'
 m = the_reg.match(content)
 print(m[1])
 print(m[2])
